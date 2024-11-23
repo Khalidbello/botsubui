3be76fadeec436917e510c74575109c7ca865a89
CodeNoJutsu <bellokhalid74@gmail.com>
2024-11-23 10:43:52 +0100

    modified laoder width

--- components/admin-dashboard/loader2.tsx
+++ components/admin-dashboard/loader2.tsx
@@ -1,13 +1,14 @@
 const LoadingAnimation: React.FC<{ h: string }> = ({ h }) => {
-    return (
-        <div>
-            <div className={`flex items-center justify-center w-full ${h} max-h-full`}>
-                <div className="animate-spin rounded-full h-full aspect-square border-t-[2px] border-b-4 border-blue-600"></div>
-            </div>
-            <div className="text-center mt-1"> loading...</div>
-        </div>
-
-    );
+  return (
+    <div>
+      <div
+        className={`flex items-center justify-center w-full ${h} max-h-full`}
+      >
+        <div className="animate-spin rounded-full h-full aspect-square border-t-[2px] border-b-[2px] border-blue-600"></div>
+      </div>
+      <div className="text-center mt-1"> loading...</div>
+    </div>
+  );
 };
 
 export default LoadingAnimation;
