93ca6f7fcaf7a4695ae20884d428dff7b9172a25
CodeNoJutsu <bellokhalid74@gmail.com>
2024-12-04 12:29:15 +0100

    Updated the route test file

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-const GET = (req: NextApiRequest) => {
+const GET = (req: NextRequest) => {
   try {
     NextResponse.json({ message: "Wops runnig fine........." });
   } catch (err) {
