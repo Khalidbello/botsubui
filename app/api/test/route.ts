48553b1c54f1b090367fd00dda99cbc3c1c9b9db
CodeNoJutsu <bellokhalid74@gmail.com>
2024-12-04 12:31:01 +0100

    Updated the route test file

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 const GET = (req: NextRequest) => {
   try {
-    NextResponse.json({ message: "Wops runnig fine........." });
+    return .json({ message: "Wops runnig fine........." });
   } catch (err) {
-    NextResponse.json({ message: "Somethin went wrong" });
+    return NextResponse.json({ message: "Somethin went wrong" });
   }
 };
 
