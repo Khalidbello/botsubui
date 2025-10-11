3859feb73f0dab91c7d92f2574c935196757e3a1
CodeNoJutsu <bellokhalid74@gmail.com>
2025-10-12 00:20:48 +0100

    Added ui to balances net  balance

--- components/admin-dashboard/statistics/balances.tsx
+++ components/admin-dashboard/statistics/balances.tsx
@@ -65,7 +65,7 @@ export default function Balances({
       )}
 
       {dataFetched && !showErrorOccured && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {/* Data Wallet Balance */}
           <div className="flex items-center bg-white border shadow-md rounded-lg p-4 hover:shadow-blue-200 transition-shadow">
             <FontAwesomeIcon
